19a6df961a29ef3aa4646161bcdddd99303b867f
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-11 14:41:22 +0500

    feat: add array of min/max instead of variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 'use strict';
-let maxRange = 20;
-let minRange = 1;
+// This array stores min and max values, if new values are set i simply add them to the end of array and use the new ones
+const arrMinMax = [1, 20];
 
+// These are default min and max values
+let minRange = arrMinMax[arrMinMax.length - 2];
+let maxRange = arrMinMax[arrMinMax.length - 1];
+
+// This variable stores the random number that user needs to guess and is reset when you restart the game
 let hiddenNumber = Math.trunc(Math.random() * maxRange) + 1;
-console.log(hiddenNumber);
