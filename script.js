8812f24c71fbef0ec2f373aee739bf8590e42b5c
Petr Kulikov <p.kulikov.dev@gmail.com>
2022-12-11 15:04:51 +0500

    feat: add element variables

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // This array stores min and max values, if new values are set i simply add them to the end of array and use the new ones
 const arrMinMax = [1, 20];
 
@@ -8,3 +9,18 @@ let maxRange = arrMinMax[arrMinMax.length - 1];
 
 // This variable stores the random number that user needs to guess and is reset when you restart the game
 let hiddenNumber = Math.trunc(Math.random() * maxRange) + 1;
+
+// These are active elements selector variables
+const restartBtn = document.querySelector('.btn-restart');
+const restartSaveBtn = document.querySelector('.btn-save');
+const submitGuessBtn = document.querySelector('.btn-submit');
+const openMenuBtn = document.querySelector('.btn-menu');
+const closeMenuBtn = document.querySelector('.btn-menu-close');
+
+// These are display-only elements selector variables
+const hiddenNumberEl = document.querySelector('.hidden-number');
+const minRangeEl = document.querySelector('.min-range');
+const maxRangeEl = document.querySelector('.max-range');
+const statusMsgEl = document.querySelector('.msg-status');
+const scoreValueEl = document.querySelector('.value-score');
+const highscoreValueEl = document.querySelector('.value-highscore');
